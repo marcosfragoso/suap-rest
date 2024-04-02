a1047c0e59a9b643be01450d6c05743f357d86d4
Marcos Vinícius <marcos.vinicius.fragoso.oliveira@ccc.ufcg.edu.br>
2024-04-02 20:57:37 -0300

    add h2 no aplication properties e dependencias no pom

--- src/main/java/edu/ifpb/web2/suaprest/model/Aluno.java
+++ src/main/java/edu/ifpb/web2/suaprest/model/Aluno.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 @Entity
+@Table(name = "Aluno")
 public class Aluno {
 
     @Id
--- src/main/java/edu/ifpb/web2/suaprest/model/Servidor.java
+++ src/main/java/edu/ifpb/web2/suaprest/model/Servidor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 @Entity
+@Table(name = "Servidor")
 public class Servidor {
 
     @Id
